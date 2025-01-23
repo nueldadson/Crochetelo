7e2d7c0fd34ba8035022c3eb61c436654c419462
Emmanuel Gideon-dada <nuel.graphix@gmail.com>
2025-01-23 23:08:34 +0100

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Button({ className = "", children = "Shop Now" }) {
 	return (
-		<a
-			href="/shop"
+		<aLink
+			to="/shop"
 			className={`px-14 py-3 text-center w-fit h-fit bg-white text-gray-800 font-semibold rounded-3xl shadow hover:bg-gray-200 transition-colors duration-300 ${className}`}
 			aria-label={children}
 		>
 			{children}
-		</a>
+		</aLink>
 	);
 }
