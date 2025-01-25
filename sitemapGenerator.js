d2da71eef82344c25ed24f9c8636b4e5aaf29e7e
Emmanuel Gideon-dada <nuel.graphix@gmail.com>
2025-01-25 09:02:50 +0100

    sitemap

--- sitemapGenerator.js
+++ sitemapGenerator.js
@@ -1,22 +1,54 @@
-import fs from 'fs';
-import path from 'path';
-import { routes } from './src/routes';
+// import fs from 'fs';
+// import path from 'path';
+// import { routes } from './src/routes';
 
-const generateSitemap = () => {
-  const baseUrl = 'https://Crochetelo.com';
-  const paths = routes.map(route => route.path);
+// const generateSitemap = () => {
+//   const baseUrl = 'https://Crochetelo.com';
+//   const paths = routes.map(route => route.path);
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+//   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+//   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+//     ${paths.map(routePath => `
+//       <url>
+//         <loc>${baseUrl}${routePath}</loc>
+//       </url>`).join('')}
+//   </urlset>`;
+
+//   const filePath = path.resolve(__dirname, './public/sitemap.xml');
+//   fs.writeFileSync(filePath, sitemap, 'utf-8');
+//   console.log(`Sitemap generated successfully at ${filePath}`);
+// };
+
+// generateSitemap();
+
+
+
+
+import fs from "fs";
+import path from "path";
+import { routes } from "./src/routes";
+
+const generateSitemap = (filename) => {
+	const baseUrl = "https://Crochetelo.com";
+	const paths = routes.map((route) => route.path);
+
+	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${paths.map(routePath => `
+    ${paths
+			.map(
+				(routePath) => `
       <url>
         <loc>${baseUrl}${routePath}</loc>
-      </url>`).join('')}
+      </url>`,
+			)
+			.join("")}
   </urlset>`;
 
-  const filePath = path.resolve(__dirname, './public/sitemap.xml');
-  fs.writeFileSync(filePath, sitemap, 'utf-8');
-  console.log(`Sitemap generated successfully at ${filePath}`);
+	const filePath = path.resolve(__dirname, `./public/${filename}`);
+	fs.writeFileSync(filePath, sitemap, "utf-8");
+	console.log(`Sitemap generated successfully at ${filePath}`);
 };
 
-generateSitemap();
+// Generate both sitemaps
+generateSitemap("sitemap.xml");
+generateSitemap("sitemap2.xml");
